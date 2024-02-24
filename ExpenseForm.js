57c34e99c61a6ad756e9ca70ad1126653e18255a
Vishal Pal <156627818+Vish7284@users.noreply.github.com>
2024-02-24 14:22:00 +0530

    submit Form
    but the input boxes are not getting empty

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -6,23 +6,60 @@ function ExpenseForm(){
     const [enteredAmount , setenteredAmount] = useState('');
     const [enteredDate , setenteredDate] = useState('');
     const [enteredLocation , setenteredLocation] = useState('');
+    // const [userInput , setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount : '',
+    //     enteredDate:'',
+    //     enteredLocation:''
+    // })
     const titleChangeHandler =(event)=>{
-        setenteredTitle(event.target.value);
+         setenteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput ,
+        //     enteredTitle :event.target.value});
+        //Handling  using the prevstate as react ensures that th evalues did nt get lost; 
+        // setUserInput((prevState)=>{
+        //     return {...prevState,enteredTitle:event.target.value}
+        // })
     }
 
     const amountChangeHandler=(event)=>{
-        setenteredAmount(event.target.value);
+          setenteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput ,
+        //     enteredAmount:event.target.value});
     }
 
     const dateChangeHandler =(event)=>{
-        setenteredDate(event.target.value);
+         setenteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate:event.target.value});
     }
 
     const locationChangeHandler =(event) =>{
-        setenteredLocation(event.target.value);
+         setenteredLocation(event.target.value);
+        // setUserInput({
+        //     ...userInput ,
+        //     enteredLocation :event.target.value});
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const ExpenseData ={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate),
+            location:enteredLocation
+        }
+        console.log(ExpenseData);
+        //Just trying to empty the input box but failed
+        // enteredTitle.event.target.value='';
+        // enteredAmount.event.target.value='';
+        // enteredDate.event.target.value='';
+        // enteredLocation.event.target.value='';
     }
   return(
-    <form>
+    <form  onSubmit={submitHandler}>
         
         <div className='new-expense__control'>
             <label for="title">Expense Title:</label>
