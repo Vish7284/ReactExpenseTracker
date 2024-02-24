a5393766b3afaf0c4609606b8a480477afb69cc8
Vishal Pal <156627818+Vish7284@users.noreply.github.com>
2024-02-24 11:26:33 +0530

    Adding buttons and {useState}

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,22 +1,32 @@
+import React, { useState } from 'react';
+import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
-import './ExpenseItem.css';
-import '../UI/Card';
 import Card from '../UI/Card';
 
 
 function ExpenseItem(props){
-    const clickHandler =() =>{
-        console.log("Delete expense")
+    const [title , setTitle] = useState(props.title);
+    const [amount ,setAmount] = useState(props.amount);
+    const eventHandler =()=>{
+        setTitle("Updated")
+        console.log(title);
     }
+
+    const eventHandler1 = ()=>{
+       setAmount("$100")
+    }
+   
     return (
-    <Card className='expense-item'>
-        <ExpenseDate date={props.date}/>
-         <ExpenseDetails title ={props.title}
-          location={props.location}
-           amount={props.amount}/>
-        <button onClick={clickHandler}>Delete Expense</button>
-    </Card>)
+        <Card className='expense-item'>
+        <div className='expense-item__date'>
+           <ExpenseDate date={props.date}/>
+            </div>
+           <ExpenseDetails title={title} amount ={amount} location ={props.location}/>
+           <button onClick={eventHandler}>Delete Expenses</button>
+           <button onClick={eventHandler1}>EditExpenses</button>
+        </Card>
+    )
 }
 
 export default ExpenseItem;
