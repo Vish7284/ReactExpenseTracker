7b73036d0381dae46cc4f74b94b5e258b6accf40
Vishal Pal <156627818+Vish7284@users.noreply.github.com>
2024-02-25 15:27:00 +0530

    Showing input at UI
    And added Expenses.js file also

---
+++ App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import NewExpense from "./NewExpense/NewExpense";
+import Expenses from "./Components/Expenses/Expenses";
+
+
+function App() {
+  const [expenses,setExpenses] = useState([
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Varanasi"
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) , location:"Varsova"},
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location:"Vadodara"
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location:"Vaishali"
+    }
+  ]);
+    const addExpenseHandler = expense =>{
+      // expenses.push(expense)
+      // console.log(expenses)
+       setExpenses(prevExpenses => {
+        return [expense,...prevExpenses];
+       })
+    }
+    return (
+    <div>
+      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses}></Expenses>
+      
+    </div>
+  );
+}
+
+export default App;
