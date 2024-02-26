484952c5265abc10e970b69d7b57484b307c660f
Vishal Pal <156627818+Vish7284@users.noreply.github.com>
2024-02-26 12:58:34 +0530

    updated with keysID

--- App.js
+++ App.js
@@ -1,33 +1,31 @@
 import { useState } from "react";
-import NewExpense from "./NewExpense/NewExpense";
+import NewExpense from "./Components/NewExpense/NewExpense";
 import Expenses from "./Components/Expenses/Expenses";
 
-
+const DUMMY_EXPENSES = [ {
+  id: 'e1',
+  title: 'Toilet Paper',
+  amount: 94.12,
+  date: new Date(2020, 7, 14),
+  location: "Varanasi"
+},
+{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) , location:"Varsova"},
+{
+  id: 'e3',
+  title: 'Car Insurance',
+  amount: 294.67,
+  date: new Date(2021, 2, 28),
+  location:"Vadodara"
+},
+{
+  id: 'e4',
+  title: 'New Desk (Wooden)',
+  amount: 450,
+  date: new Date(2021, 5, 12),
+  location:"Vaishali"
+}]
 function App() {
-  const [expenses,setExpenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "Varanasi"
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) , location:"Varsova"},
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location:"Vadodara"
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location:"Vaishali"
-    }
-  ]);
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
     const addExpenseHandler = expense =>{
       // expenses.push(expense)
       // console.log(expenses)
